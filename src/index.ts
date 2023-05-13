f23c19234112f1cd390e4086081fb83d6632388a
Pierre Dahmani <hi@pierre-dev.com>
2023-05-13 16:22:19 +0200

    adds additional output.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 const main = () => {
   console.log("hello Node.js and Typescript world :]");
+  console.log(
+    "This PR is just here to showcase the Github Action pipeline run on PR."
+  );
 };
 
 main();
