fce1d747dce7accad57304d31ce3091198a1ed01
Pierre Dahmani <hi@pierre-dev.com>
2023-05-16 23:10:17 +0200

    comment out the eslint-error provoking code to fix github action

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ const main = () => {
   console.log("hello Node.js and Typescript world :]");
 };
 
-// This is just here to force a linting error for now to demonstrate/test the
-// eslint pipeline.
-const x: number[] = [1, 2];
-const y: Array<number> = [3, 4];
+// This was just here to force a linting error for now to demonstrate/test the
+// eslint pipeline. You can uncomment this and run "yarn check-lint" to test the
+// linting.
+// const x: number[] = [1, 2];
+// const y: Array<number> = [3, 4];
 
-// This is just here to force a linting error for now to demonstrate/test the
-// eslint pipeline.
-if (x == y) {
-  console.log("equal!");
-}
+// This was just here to force a linting error for now to demonstrate/test the
+// eslint pipeline. You can uncomment this and run "yarn check-lint" to test the
+// linting.
+// if (x == y) {
+//   console.log("equal!");
+// }
 
 main();
