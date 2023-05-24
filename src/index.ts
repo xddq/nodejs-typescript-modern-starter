efe362ea09013df952978d5a22a12eba0850b318
Pierre Dahmani <hi@pierre-dev.com>
2023-05-24 21:31:33 +0200

    updates package.json with cli/bin stuff - updates tsconfig with debugging, type declaration and json imports.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+// NOTE: You can remove the first line if you don't plan to release an
+// executable package. E.g. code that can be used as cli like prettier or eslint
+
 const main = () => {
   console.log("hello Node.js and Typescript world :]");
 };
